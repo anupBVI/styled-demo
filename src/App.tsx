ab796bd3b5ea8a87631f58705f545d3f658c72b2
Anup Manwatkar <anup.manwatkar@brainvire.com>
2022-12-22 10:14:51 +0530

    Styled Demo todo app Initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import "../src/App.css";
+import { Body, Content, ContentBody, ContentHeader, Header, Layout, SubTitle, Title } from "./components/Layout";
+import { ITask } from "./Interfaces/interface";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import { Theme } from "./styles/Theme";
 
-function App() {
+
+const App = () => {
+
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [list, setList] = useState<ITask[]>([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    let { name, value } = e.target;
+    console.log(name);
+    if (name === "task") {
+      setTask(value);
+    } else {
+      setDeadline(+value);
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+    const newTask = { taskName: task, deadline: deadline };
+    if (task !== "") {
+      setList([...list, newTask]);
+      setTask("");
+
+      setTimeout(() => {
+        alert("New Task created");
+      }, 100);
+      setDeadline(0);
+    } else {
+      alert("task name should not be empty");
+    }
+  };
+
+  const deleteTask = (taskNameToDelete: string): void => {
+    setList(
+      list.filter((task) => {
+        return task.taskName != taskNameToDelete;
+      })
+    );
+    setTimeout(() => {
+      alert("Task Deleted successfully");
+    }, 100);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+
+      <Layout>
+        <Header>
+          <Title>Welcome to Task Manager</Title>
+
+          <form onSubmit={handleSubmit}>
+            <div>
+              <input
+                name="task"
+                value={task}
+                type="text"
+                placeholder="Enter Task Name"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <input
+                name="deadline"
+                value={deadline}
+                type="number"
+                placeholder="Enter Task Deadline"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <button type="submit">Add Task</button>
+            </div>
+          </form>
+        </Header>
+
+        <Content>
+          {
+            list.length !== 0 ? <Body>
+              <ContentHeader>
+                <div>Task Name</div>
+                <div>Task Deadline</div>
+                <div>Action</div>
+              </ContentHeader>
+
+
+              {
+                list.map((item, index) => {
+                  return (
+
+                    <ContentBody key={index}>
+                      <div>{item.taskName}</div>
+                      <div>{item.deadline} {item.deadline > 1 ? "Days" : "Day"}</div>
+                      <div>
+                        <button>Complete</button>
+                        <button onClick={() => { deleteTask(item.taskName) }}>Delete</button>
+                      </div>
+                    </ContentBody>
+                  )
+                })
+              }
+
+            </Body> : <SubTitle>No Tasks available</SubTitle>
+          }
+
+        </Content>
+      </Layout>
+
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,155 @@
+import styled from "styled-components";
+import { Theme } from './../styles/Theme';
+
+export const Layout = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background: #0f3f56;
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+`;
+export const Title = styled.h2`
+  color:white;
+  /* font-family: 'Poppins', sans-serif; */
+  font-family:${()=>Theme.font.primary};
+  font-weight:${()=>Theme.font.extrabold};
+  text-shadow: 4px 4px 2px rgba(0,0,0,0.6);
+  letter-spacing:.1rem;
+`
+export const SubTitle = styled.h2`
+  color:white;
+  /* font-family: 'Poppins', sans-serif; */
+  font-family:${()=>Theme.font.primary};
+  font-weight:${()=>Theme.font.regular};
+  text-shadow: 4px 4px 2px rgba(0,0,0,0.6);
+  letter-spacing:.1rem;
+`
+
+
+export const Header = styled.header`
+  width: 80%;
+ 
+  /* background: blue; */
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 12px;
+  form {
+    width: 60%;
+    /* background:red; */
+    padding: 4px 12px;
+    display: flex;
+    gap: 12px;
+    div {
+      &:nth-child(1) {
+        flex: 3;
+      }
+      &:nth-child(2) {
+        flex: 1;
+      }
+      input,
+      button {
+        width: 100%;
+        padding:10px 6px;
+      }
+      input {
+        outline: none;
+      }
+    }
+  }
+`;
+export const Content = styled.section`
+  width: 80%;
+  /* background: green; */
+  /* height: 400px; */
+  display: flex;
+  justify-content: center;
+  padding: 12px;
+`;
+
+export const Body = styled.section`
+  width: 80%;
+  background: #31ccff;
+  border-radius:10px;
+  display: flex;
+  flex-direction:column;
+  gap:.5rem;
+  align-items: center;
+  padding: 12px;
+`;
+export const ContentHeader = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 3px;
+  justify-content:center;
+  padding: 4px;
+  color:white;
+  font-family:${()=>Theme.font.primary};
+  font-weight:${()=>Theme.font.bold};
+  letter-spacing:.1rem;
+  div {
+    background: #132c58;
+    display: flex;
+    padding:8px;
+    justify-content: center;
+    align-items: center;
+    &:nth-child(1) {
+      flex: 2;
+    }
+    &:nth-child(2) {
+      flex: 2;
+    }
+    &:nth-child(3) {
+      flex: 1;
+    }
+  }
+`;
+export const ContentBody = styled.div`
+  width: 100%;
+  width: 100%;
+  display: flex;
+  gap: 3px;
+  justify-content:center;
+  padding: 5px;
+  font-family:${()=>Theme.font.primary};
+  font-weight:${()=>Theme.font.regular};
+  div {
+    background: #2d53b0;;
+    display: flex;
+    padding: 8px 4px;
+    justify-content: center;
+    align-items: center;
+    color:white;
+    &:nth-child(1) {
+      flex: 2;
+    }
+    &:nth-child(2) {
+      flex: 2;
+    }
+    &:nth-child(3) {
+      flex: 1;
+      display:flex;
+      gap:4px;
+    }
+    button{
+        background-color:#0fd5c2;
+        border:none;
+        color:white;
+        padding:6px 12px;
+        cursor:pointer;
+        &:nth-child(2){
+            background-color:#ff3870
+        }
+
+
+
+      }
+    
+  }
+`;
